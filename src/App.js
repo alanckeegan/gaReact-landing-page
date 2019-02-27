eb4aa99beae40cfa2f34765c9ee482a0e0425f6e
Alan Keegan <alan.c.keegan@gmail.com>
2019-02-27 18:18:38 -0500

    first push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import { Button } from 'reactstrap';
+import logo from './assets/keenLogo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <div>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <img src={logo} alt="logo" />
+        <h1 className="Tagline">find out if they like you back</h1>
+        <button> Button </button>
       </div>
+      <div className="Filler">
+      </div>
+      </div>
+
+
+
+
     );
   }
 }
